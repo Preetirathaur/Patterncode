16e34c277cc12498e46752bf04a7e4497721e0f4
Preeti Rathaur <123716002+Preetirathaur@users.noreply.github.com>
2023-07-11 18:29:46 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,10 @@
+class pattern{
+    public static void main( string []args){
+        for(int line=0;line<5;line++){
+            for(int star=1;star<=line;star++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+}
